e8be19f4b4b080f60848fcfdeb6ab6becd33a3f0
anishLearnsToCode <anish_@outlook.com>
2021-01-13 20:51:29 +0530

    solves day 4

---
+++ src/Day4.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Day4 {
+
+}
+
+class Person {
+    private int age;
+
+    public Person(int initialAge) {
+        this.age = Math.max(initialAge, 0);
+        if (initialAge < 0) {
+            System.out.println("Age is not valid, setting age to 0.");
+        }
+    }
+
+    public void amIOld() {
+        // Write code determining if this person's age is old and print the correct statement:
+        if (this.age < 13) {
+            System.out.println("You are young.");
+        } else if (this.age < 18) {
+            System.out.println("You are a teenager.");
+        } else {
+            System.out.println("You are old.");
+        }
+    }
+
+    public void yearPasses() {
+        this.age++;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int i = 0; i < T; i++) {
+            int age = sc.nextInt();
+            Person p = new Person(age);
+            p.amIOld();
+            for (int j = 0; j < 3; j++) {
+                p.yearPasses();
+            }
+            p.amIOld();
+            System.out.println();
+        }
+        sc.close();
+    }
+}
